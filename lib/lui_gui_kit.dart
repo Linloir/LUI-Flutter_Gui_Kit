e66a7031c16da67d574a002225087dcd98a2f67f
Linloir <3145078758@qq.com>
2022-04-08 17:01:19 +0800

    First commit

---
+++ lib/lui_gui_kit.dart
@@ -0,0 +1,15 @@
+/*
+ * @Author       : Linloir
+ * @Date         : 2022-04-08 15:58:37
+ * @LastEditTime : 2022-04-08 16:57:39
+ * @Description  : Main
+ */
+library lui_gui_kit;
+
+export 'src/general/buttons/flat_button.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/src/general/buttons/flat_button.dart
@@ -0,0 +1,10 @@
+/*
+ * @Author       : Linloir
+ * @Date         : 2022-04-08 16:52:46
+ * @LastEditTime : 2022-04-08 16:54:03
+ * @Description  : Flat Button Class
+ */
+
+class LuiFlatButton {
+  
+}
