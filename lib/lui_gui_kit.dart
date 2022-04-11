f6815ad6df122a3009af9d2ac1a2a2f5ef7ed0c3
Linloir <3145078758@qq.com>
2022-04-11 14:15:43 +0800

    add: (progress_bar) add progress bar

--- lib/lui_gui_kit.dart
+++ lib/lui_gui_kit.dart
@@ -1,7 +1,7 @@
 /*
  * @Author       : Linloir
  * @Date         : 2022-04-08 15:58:37
- * @LastEditTime : 2022-04-10 15:51:17
+ * @LastEditTime : 2022-04-11 14:15:29
  * @Description  : Main
  */
 library lui_gui_kit;
@@ -9,3 +9,4 @@ library lui_gui_kit;
 export 'src/general/buttons/flat_button.dart';
 export 'src/data_entry/check_box.dart';
 export 'src/data_display/badge.dart';
+export 'src/data_display/progress_bar.dart';
--- lib/src/data_display/progress_bar.dart
+++ lib/src/data_display/progress_bar.dart
@@ -4,3 +4,49 @@
  * @LastEditTime : 2022-04-11 14:14:35
  * @Description  : 
  */
+
+import 'package:flutter/material.dart';
+
+class LuiLinearProgressBar extends StatefulWidget {
+  const LuiLinearProgressBar({Key? key, required this.progress, this.backgroundColor, this.borderColor, this.highlightColor, this.borderWidth}) : super(key: key);
+
+  final double progress;
+  final Color? backgroundColor;
+  final Color? borderColor;
+  final Color? highlightColor;
+  final double? borderWidth;
+
+  @override
+  State<LuiLinearProgressBar> createState() => _LuiLinearProgressBar();
+}
+
+class _LuiLinearProgressBar extends State<LuiLinearProgressBar> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(constraints.maxHeight / 2),
+          child: Container(
+            decoration: BoxDecoration(
+              color: widget.backgroundColor ?? Colors.grey[100]!,
+              borderRadius: BorderRadius.circular(constraints.maxHeight / 2),
+              border: Border.all(color: widget.borderColor ?? Colors.grey[100]!, width: widget.borderWidth ?? 2.0)
+            ),
+            alignment: Alignment.centerLeft,
+            width: double.infinity,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 750),
+              curve: Curves.easeInOutExpo,
+              width: constraints.maxWidth * widget.progress,
+              decoration: BoxDecoration(
+                color: widget.highlightColor ?? Colors.teal[300]!,
+                borderRadius: BorderRadius.circular(constraints.maxHeight / 2),
+              ),
+            ),
+          ),
+        );
+      }
+    );
+  }
+}
